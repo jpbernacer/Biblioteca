2ea041e949917111c4f309cab47bb39f3dafbc70
Juan Pablo Bernacer <juanpablo.bernacertapia@usp.ceu.es>
2024-10-30 18:27:58 +0100

    Código comentado

--- src/negocio/Biblioteca.java
+++ src/negocio/Biblioteca.java
@@ -13,9 +13,9 @@ public class Biblioteca{
         cargarLibros();
     }
     private void cargarLibros(){
-        // Se verifica que Scanner sea null como valor inicial
+        // Se inicia el Scanner y se verifica que sea null como valor inicial
         Scanner sc = null;
-        
+
         try {
             // Se inicializa File y se le adjudica el nombre fichero.
             File fichero = new File ("biblioteca.csv");
@@ -38,35 +38,44 @@ public class Biblioteca{
             }
 
         }catch(IOException ex){
+            //Impresión de error en caso de que no se pueda cargar el fichero
 			System.out.println("Error en la lectura del fichero de libros.");
 			System.out.println("A continuación se muestra más información:");
 			System.out.println(ex);
 		}finally{
+            // Si el Scanner no es null, se cierra
 			if (sc != null) sc.close();
 		}
 
 	}
 
     public void annadir(Libro libro){
+       // Método para añadir un libro a la lista de libros y volcarlos existentes al programa
         listaLibros.add(libro);
         volcarLibros();
     }
 
     private void volcarLibros(){
+        // Inicialización de FileWriter
         FileWriter fw = null;
         try{
+            // Se inicializa el FileWriter para el fichero biblioteca.csv
             fw = new FileWriter("biblioteca.csv");
+            // Por cada libro en la lista de libros se añade un nuevo libro
             for(Libro libro : listaLibros){
                 fw.write(libro.getTitulo() + "," + libro.getAutor() + "," + libro.getFecha_publi() + "," + libro.getNumero_id() + "," + libro.getDisponible() + "\n");
             }
         }catch(IOException ex){
+            //Impresión de error en caso de que no se pueda añadir un nuevo libro
             System.out.println("No se ha podido añadir el nuevo libro. Error en la escritura del fichero");
             System.out.println("A continuación se muestra más información:");
             System.out.println(ex);
         }finally{
             try{
+                // Si el FileWriter no es null, se cierra
                 if (fw != null) fw.close();
             }catch(IOException ex){
+                //Impresión de error en caso de que no se pueda cerrar el FileWriter
                 System.out.println(ex);
             }
         }
@@ -74,7 +83,10 @@ public class Biblioteca{
 
     @Override
     public String toString(){
+        // Método para imprimir los datos de los libros
+        // Se inicializa un StringBuilder para concatenar los datos de los libros
         StringBuilder strLibros = new StringBuilder();
+        // Por cada libro en la lista de libros se añade un nuevo libro
         for(Libro libro : listaLibros) strLibros.append(libro + "\n");
         return strLibros.toString();
     }
