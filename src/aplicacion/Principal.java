c0bb632e1ce7ba2efde1f5f8327be89f189f170b
Juan Pablo Bernacer <juanpablo.bernacertapia@usp.ceu.es>
2024-11-01 13:33:07 +0100

    Añadida documentación y comentarios en la clase Principal

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,9 +1,26 @@
+/**
+ * Clase Principal que inicia la aplicación de gestión de la biblioteca.
+ * 
+ * Código creado por Juan Pablo Bernacer 
+ * Escrito en Español
+ * 30/10/2024
+ */
+
 package aplicacion;
 
 import presentacion.InterfazUsuario;
 
-public class Principal{
-	public static void main(String[] args){
-		InterfazUsuario.ejecutar(args);
-	}
+/**
+ * La clase Principal contiene el método main que inicia la ejecución de la aplicación.
+ */
+public class Principal {
+    /**
+     * El método main es el punto de entrada de la aplicación.
+     * Llama al método ejecutar de la clase InterfazUsuario para iniciar la interacción con el usuario.
+     * 
+     * @param args Los argumentos de la línea de comandos.
+     */
+    public static void main(String[] args) {
+        InterfazUsuario.ejecutar(args);
+    }
 }
