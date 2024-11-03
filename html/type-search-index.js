e704ef153a7e1843c32f72e4f1fda5d73859ede0
Juan Pablo Bernacer <juanpablo.bernacertapia@usp.ceu.es>
2024-11-03 21:14:11 +0100

    Corrección documentación en base a la optimización

---
+++ html/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"negocio","l":"Biblioteca"},{"p":"presentacion","l":"InterfazUsuario"},{"p":"negocio","l":"Libro"},{"p":"aplicacion","l":"Principal"}];updateSearchResults();
